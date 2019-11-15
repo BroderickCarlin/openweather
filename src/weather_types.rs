3b69cfa65e83fce9ba657f89ab570c5d92e28c0c
Christoph <christoph.gross@student.uni-tuebingen.de>
2019-11-15 11:08:04 +0100

    - add default and partialeq to all weather_types - make Wind:deg optional - add WeatherReportCurrent::timezone

--- src/weather_types.rs
+++ src/weather_types.rs
@@ -1,12 +1,12 @@
 use serde_derive::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Default)]
 pub struct Coordinates {
     pub lat: f32,
     pub lon: f32,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Default)]
 pub struct CityShort {
     pub id: u32,
     pub name: String,
@@ -18,7 +18,7 @@ pub struct CityShort {
     pub sunset: u64,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Default)]
 pub struct CityLong {
     pub geoname_id: u64,
     pub name: String,
@@ -31,7 +31,7 @@ pub struct CityLong {
     pub population: u32,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Default)]
 pub struct Main {
     pub temp: f32,
     pub temp_min: f32,
@@ -46,7 +46,7 @@ pub struct Main {
     pub temp_kf: Option<f32>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Default)]
 pub struct Weather {
     pub id: u32,
     pub main: String,
@@ -54,38 +54,38 @@ pub struct Weather {
     pub icon: String,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Default)]
 pub struct Clouds {
     pub all: i32,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Default)]
 pub struct Wind {
     pub speed: f32,
-    pub deg: f32,
+    pub deg: Option<f32>,
     pub gust: Option<f32>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Default)]
 pub struct Rain {
     /// Rain volume in mm
     #[serde(rename = "3h")]
     pub three_h: Option<f32>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Default)]
 pub struct Snow {
     /// Snow volume
     #[serde(rename = "3h")]
     pub three_h: Option<f32>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Default)]
 pub struct System {
     pub pod: String,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Default)]
 pub struct TempDaily {
     pub day: f32,
     pub min: f32,
@@ -95,7 +95,7 @@ pub struct TempDaily {
     pub morn: f32,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Default)]
 pub struct TimeSliceHourly {
     pub dt: u64,
     pub main: Main,
@@ -108,7 +108,7 @@ pub struct TimeSliceHourly {
     pub dt_txt: String,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Default)]
 pub struct TimeSliceDaily {
     pub dt: u64,
     pub temp: TempDaily,
@@ -120,13 +120,13 @@ pub struct TimeSliceDaily {
     pub clouds: i32,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Default)]
 pub struct ErrorReport {
     pub cod: u32,
     pub message: String,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Default)]
 pub struct Sys {
     #[serde(rename = "type")]
     pub message_type: u32,
@@ -137,7 +137,7 @@ pub struct Sys {
     pub sunset: u64,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Default)]
 pub struct WeatherReportCurrent {
     pub coord: Coordinates,
     pub weather: Vec<Weather>,
@@ -148,12 +148,13 @@ pub struct WeatherReportCurrent {
     pub clouds: Clouds,
     pub dt: u64,
     pub sys: Sys,
+    pub timezone: Option<i32>,
     pub id: u64,
     pub name: String,
     pub cod: u16,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Default)]
 pub struct WeatherReport5Day {
     pub cod: String,
     pub message: f32,
@@ -162,7 +163,7 @@ pub struct WeatherReport5Day {
     pub city: CityShort,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Default)]
 pub struct WeatherReport16Day {
     pub cod: String,
     pub message: f32,
@@ -171,7 +172,7 @@ pub struct WeatherReport16Day {
     pub list: Vec<TimeSliceDaily>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Default)]
 pub struct WeatherReportHistoricalElement {
     pub main: Main,
     pub wind: Wind,
@@ -180,7 +181,7 @@ pub struct WeatherReportHistoricalElement {
     pub dt: u64,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Default)]
 pub struct WeatherReportHistorical {
     pub message: String,
     pub cod: String,
@@ -190,14 +191,14 @@ pub struct WeatherReportHistorical {
     pub list: Vec<WeatherReportHistoricalElement>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Default)]
 pub struct WeatherAccumulatedTemperatureElement {
     pub date: String,
     pub temp: f32,
     pub count: u32,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Default)]
 pub struct WeatherAccumulatedPrecipitation {
     pub message: String,
     pub cod: String,
@@ -206,14 +207,14 @@ pub struct WeatherAccumulatedPrecipitation {
     pub list: Vec<WeatherAccumulatedPrecipitationElement>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Default)]
 pub struct WeatherAccumulatedPrecipitationElement {
     pub date: String,
     pub rain: f32,
     pub count: u32,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Default)]
 pub struct WeatherAccumulatedTemperature {
     pub message: String,
     pub cod: String,
@@ -222,7 +223,7 @@ pub struct WeatherAccumulatedTemperature {
     pub list: Vec<WeatherAccumulatedTemperatureElement>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Default)]
 pub struct UvIndex {
     pub lat: f32,
     pub lon: f32,
@@ -231,12 +232,12 @@ pub struct UvIndex {
     pub value: f32,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Default)]
 pub struct ForecastUvIndex {
     pub list: Vec<UvIndex>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Default)]
 pub struct HistoricalUvIndexElement {
     pub lat: f32,
     pub lon: f32,
@@ -245,7 +246,86 @@ pub struct HistoricalUvIndexElement {
     pub value: u32,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Default)]
 pub struct HistoricalUvIndex {
     pub list: Vec<HistoricalUvIndexElement>,
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    /// This test/derive once failed
+    fn current_weather_derive_failure_1() {
+        let der_string = "{\"coord\":{\"lon\":10.45,\"lat\":49.57},\"weather\":[{\"id\":801,\"main\":\"Clouds\",\"description\":\"Ein paar Wolken\",\"icon\":\"02d\"}],\"base\":\"stations\",\"main\":{\"temp\":2.94,\"pressure\":998,\"humidity\":86,\"temp_min\":1.67,\"temp_max\":3.89},\"visibility\":10000,\"wind\":{\"speed\":1},\"clouds\":{\"all\":20},\"dt\":1573810268,\"sys\":{\"type\":1,\"id\":1274,\"country\":\"DE\",\"sunrise\":1573799471,\"sunset\":1573832742},\"timezone\":3600,\"id\":2820859,\"name\":\"Berlin\",\"cod\":200}";
+
+        let weather_report: WeatherReportCurrent =
+            serde_json::from_str(&der_string).expect("current weather derive failure testcase 1");
+
+        let weather_report_derived = WeatherReportCurrent {
+            coord: Coordinates {
+                lon: 10.45,
+                lat: 49.57,
+            },
+            weather: vec![Weather {
+                id: 801,
+                main: "Clouds".to_string(),
+                description: "Ein paar Wolken".to_string(),
+                icon: "02d".to_string(),
+            }],
+            base: "stations".to_string(),
+            main: Main {
+                temp: 2.94,
+                pressure: 998.0,
+                humidity: 86.0,
+                temp_min: 1.67,
+                temp_max: 3.89,
+                ..Default::default()
+            },
+            visibility: 10000,
+            wind: Wind {
+                speed: 1.0,
+                ..Default::default()
+            },
+            clouds: Clouds {
+                all: 20,
+                ..Default::default()
+            },
+            dt: 1573810268,
+            sys: Sys {
+                message_type: 1,
+                id: 1274,
+                country: "DE".to_string(),
+                sunrise: 1573799471,
+                sunset: 1573832742,
+                ..Default::default()
+            },
+            timezone: Some(3600),
+            id: 2820859,
+            name: "Berlin".to_string(),
+            cod: 200,
+
+            ..Default::default()
+        };
+
+        assert_eq!(weather_report.timezone, Some(3600));
+        assert_eq!(
+            weather_report.clouds,
+            Clouds {
+                all: 20,
+                ..Default::default()
+            }
+        );
+
+        assert_eq!(
+            weather_report.wind,
+            Wind {
+                speed: 1.0,
+                ..Default::default()
+            }
+        );
+
+        assert_eq!(weather_report, weather_report_derived);
+    }
 }
